ce0ec6fc3d700aa7cd3e04b3192d1b1ccb4ee9cb
Code Ninja <fbook3603@gmail.com>
2022-11-01 14:00:52 +0600

    search process add

--- js/dile.js
+++ js/dile.js
@@ -1,12 +1,31 @@
 // JSON data section
 const PhoneSearch = (phoneText, dataLimit) => {
   const url = `https://openapi.programming-hero.com/api/phones?search=${phoneText}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.data, dataLimit));
+  spinnerAdd();
 };
 
 // Input value
 const searchValue = (dataLimit) => {
   const searchText = document.getElementById("searchInput").value;
+  PhoneSearch(searchText, dataLimit);
+  spinnerAdd(true);
 };
+
+// input data search
+document
+  .getElementById("searchInput")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      searchValue(10);
+    }
+  });
+
+document.getElementById("searchBtn").addEventListener("click", function () {
+  searchValue(10);
+});
 
 // spinner
 const spinnerAdd = (value) => {
